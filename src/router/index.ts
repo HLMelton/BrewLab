e08a41b152317b643af95539d3fbb4c0e6dd2260
Hunter L. Melton <hunterlmelton@gmail.com>
2023-07-28 10:01:06 -0700

    router and programmatic routing changes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,11 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import TabsPage from '../views/TabsPage.vue'
 
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/Brews'
+    redirect: '/tabs/brews'
   },
   {
     path: '/tabs/',
@@ -13,18 +14,24 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/Brews'
+        redirect: '/tabs/brews'
       },
       {
-        path: 'Brews',
-        component: () => import('@/views/Brews.vue')
+        path: '/brews',
+        component: () => import('@/views/Brews.vue'),
+        children: [
+          {
+            path:'/brews/:id',
+            component: () => import('../views/BrewRecipe.vue')
+          }
+        ],
       },
       {
         path: 'Export',
         component: () => import('@/views/Export.vue')
       },
       {
-        path: 'Profile',
+        path: 'tab3',
         component: () => import('@/views/Profile.vue')
       }
     ]
