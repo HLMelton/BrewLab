09f09e30ecffb4bb87cb6b3cb157bb7ab847fd24
HLMelton <hunterlmelton@gmail.com>
2024-02-23 12:21:02 -0700

    newbrew changes

--- src/views/NewBrew.vue
+++ src/views/NewBrew.vue
@@ -1,13 +1,16 @@
 <template>
   <ion-page>
-    <ion-content class="ion-padding">
-      <ion-list lines="none">
+    <ion-content>
+
+      <ion-list lines="none" id="creationCard" class="ion-padding">
+        <div id="headerText">Brew Prep</div>
         <ion-item>
           <ion-input label="Brew Title" v-model="brewTemplate.title"> </ion-input>
         </ion-item>
 
         <ion-item>
-          <ion-range :ticks="true" :snaps="true" :min="1" :max="22" :pin="true" label-placement="start" label="Ratio" v-model="brewTemplate.ratio" :pin-formatter="(value: number)=>`1:${value}`"></ion-range>
+          <ion-text class="ion-justify-content-center">Ratio 1:{{ brewTemplate.ratio }} </ion-text>
+          <ion-range :ticks="true" :snaps="true" :min="1" :max="22" label-placement="start" aria-label="Ratio" v-model="brewTemplate.ratio" class="ion-padding"></ion-range>
         </ion-item>
 
         <ion-item>
@@ -35,54 +38,45 @@
             <ion-select-option value="Cold Brew">Cold Brew</ion-select-option>
           </ion-select>
         </ion-item>
-        <!-- <ion-item> -->
           <ion-grid>
-            <ion-row class="ion-justify-content-center">
-
-              <ion-col>
+            <ion-row>
+              <ion-col class="ion-padding">
                 <ion-input maxlength="3" label="Input Weight:" v-model="brewTemplate.inputWeight"></ion-input>
               </ion-col>
 
-              <ion-col class="ion-padding ion-text-center">
-                <!-- <div id="targetWeight"> -->
+              <ion-col class="ion-padding-top ion-margin-top">
                   <ion-toggle  label-placement="start" v-model="brewTemplate.bloom">Bloom</ion-toggle>
-                <!-- </div> -->
               </ion-col>
 
-              <ion-col>
-                <div id="targetWeight">
-                  <p>Target Weight:  {{calculateTarget(brewTemplate.inputWeight, brewTemplate.ratio) }} </p>
-                </div>
+              <ion-col class="ion-padding-end ion-padding-top ion-margin-top">
+                  <ion-text>Target Weight:  {{calculateTarget(brewTemplate.inputWeight, brewTemplate.ratio) }} </ion-text>
               </ion-col>
 
             </ion-row>
           </ion-grid>
-
-
-
-
-        <!-- </ion-item> -->
 
         <ion-item v-if="brewTemplate.bloom === true">
           <ion-input label="Bloom Ratio"></ion-input>
           <ion-input label="Duration"></ion-input>
         </ion-item>
 
-        <ion-item v-for="step in brewTemplate.brewSteps">
+        <ion-item v-for="step in brewTemplate.brewSteps" >
           <ion-input label="Weight Target" v-model="step.weight"></ion-input>
           <ion-input label="Timing"></ion-input>
+
         </ion-item>
 
         <ion-item class="ion-justify-center">
-          <ion-button @click="addTemplateStep()">Add Step</ion-button>
+          <ion-button @click="addTemplateStep()" slot="start">Add Step</ion-button>
+          <ion-button @click="removeTemplateStep()" slot="end"> Remove</ion-button>
         </ion-item>
 
         <ion-item>
           <ion-input v-model="brewTemplate.additionalNotes" label="Additional Notes" class="ion-padding"></ion-input>
         </ion-item>
 
-        <ion-item class="ion-justify-content-center">
-          <ion-button slot="end" :disabled="false" @click="console.log(brewTemplate)"> Save </ion-button>
+        <ion-item class="ion-text-center">
+                <ion-button :disabled="false" @click="console.log(brewTemplate)">Save</ion-button>
         </ion-item>
 
       </ion-list>
@@ -96,6 +90,7 @@ import { useRouter } from 'vue-router';
 import { supabase } from '../supabase';
 import { BrewDetail } from './BrewList.vue';
 import { ref } from 'vue';
+import { createGesture } from '@ionic/vue';
 
 const router = useRouter();
 
@@ -128,6 +123,10 @@ async function commitBrew(brewTemplate: BrewDetail){
   console.log(brewTemplate)
 }
 
+// const swipeToRemoveStep = createGesture({
+
+// })
+
 </script>
 
 
@@ -138,4 +137,14 @@ async function commitBrew(brewTemplate: BrewDetail){
   text-align: center;
   justify-content: center;
 }
+
+#creationCard{
+  border-radius: 2rem;
+}
+
+#headerText{
+  text-align: center;
+  font-family: 'Gerhaus';
+};
+
 </style>
