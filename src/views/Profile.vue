97eef9e112fe8075a99e4b5547947ec2ecfcf2b6
HLMelton <hunterlmelton@gmail.com>
2024-02-22 17:03:23 -0800

    Sign out function refactor

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,32 +2,48 @@
   <ion-page>
     <ion-content>
       <ion-card class="ion-padding ion-margin" id="profileCard"> 
-        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-        <ion-card-title>Username</ion-card-title>
+        <img src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <ion-card-title class=" ion-padding">Username</ion-card-title>
+        <ion-card-subtitle class=" ion-padding-start">Test</ion-card-subtitle>
         <ion-card-content>
-          User Metadata such as Brews made will be here
+           <!-- Email: {{ userInfo.confirmed_at }} -->
         </ion-card-content>
       </ion-card>
+      <!-- <ion-button @click="console.log(userInfo)" class="ion-padding" expand="block"> Log User </ion-button> -->
       <ion-button @click="handleSignOut" class="ion-padding" expand="block"> Sign Out </ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { loadingController, toastController } from '@ionic/vue';
 import { supabase } from '../supabase';
 import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
 const router = useRouter();
 
-const userInfo = ref('')
+async function handleSignOut(){
+  const loader = await loadingController.create({})
+  const toast = await toastController.create({})
 
-const handleSignOut = async() => {
-  const { error } = await supabase.auth.signOut()
-  console.log('Signed out')
-  router.push('/authentication')
+  try{
+    const { error } = await supabase.auth.signOut()
+  } catch(error: any){
+    toast.message = error.error_description || error.message
+    await toast.present()
+  }finally{
+    await loader.dismiss()
+    router.push('/authentication')
+  }
 }
+
+// const handleSignOut = async() => {
+//   const { error } = await supabase.auth.signOut()
+//   console.log('Signed out')
+//   router.push('/authentication')
+// }
 
 async function getUser(){
   const currentUser = await supabase.auth.getUser()
